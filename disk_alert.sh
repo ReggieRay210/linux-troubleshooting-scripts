33607e8831757e49984a3b9af0b02b02a4e72603
Reginald Griffin II <74481504+ReggieRay210@users.noreply.github.com>
2026-06-12 19:07:06 -0500

    Fix log file path in disk_alert.sh

--- disk_alert.sh
+++ disk_alert.sh
@@ -2,7 +2,7 @@
 
 # =====================================================================================
 # disk_alert.sh
-# Check the systems disk usage of /(root directory) or any mount point.
+# Check the system's disk usage of /(root directory) or any mount point.
 # If the usage exceeds 80%, write a warning to the disk_alert.log with a timestamp.
 # If the usage is below 80%, print to the terminal 'Disk Ok'.
 # Usage: ./disk_alert.sh [argument]
@@ -12,7 +12,7 @@ TARGET="${1:-/}"
 disk_usage_percent=$(df -h "${TARGET}" | awk 'NR==2 {print $5}'| tr -d '%')
 
 if [ "${disk_usage_percent}" -gt 80 ]; then
-        echo -e "$(date) \033[0;33mWARNING: Disk Usage on ${TARGET} is ${disk_usage_percent}%." >> disk_alert.log
+        echo -e "$(date) \033[0;33mWARNING: Disk Usage on ${TARGET} is ${disk_usage_percent}%." >> ./disk_alert.log
 else
         echo -e "$(date) \033[0;32mINFO: Disk OK at ${disk_usage_percent}%."
 fi
