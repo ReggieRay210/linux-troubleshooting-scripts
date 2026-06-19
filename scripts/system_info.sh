8c4f63e249de26b4d33e1dd406a18e03e5d9ded4
Reginald Griffin II <74481504+ReggieRay210@users.noreply.github.com>
2026-06-19 17:19:37 -0500

    Revise header comments in system_info.sh
    Updated script header comments for clarity and consistency.

--- scripts/system_info.sh
+++ scripts/system_info.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
-
-# System_Info
-# Displays system information: date, uptime, current logged-in user, memory, and disk usage.
+#################################
+#
+# Name: system_Info.sh
+#
+# Description: displays system information: uptime, current logged-in user, memory, and disk usage.
+#
 # Usage: ./system_info
-
+#################################
 
 uptime_format=$(uptime -p | sed 's/^up //')
 disk_percent=$(df -h / | awk 'NR==2 {print $5}')
 disk_used_and_total=$(df -h / | awk 'NR==2 {print $3"/"$2}')
 
-
-echo "=== System Information ==="
-echo "Date: $(date) "
 echo "Uptime: ${uptime_format}"
 echo "User: $(whoami)"
 echo "Memory: $(free -h | awk '/^Mem:/ {print $3 "/" $2}')"
