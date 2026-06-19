f51cfee79d21ee9d2cc68539f52b9e8abf1c94d9
Reginald Griffin II <74481504+ReggieRay210@users.noreply.github.com>
2026-06-19 17:17:59 -0500

    Enhance network_check.sh with better output formatting
    Added a modification to improve interaction with the summary report script.

--- scripts/network_check.sh
+++ scripts/network_check.sh
@@ -2,6 +2,7 @@
 
 ######################################
 # File name: network_check.sh
+#
 # Description: Automates a basic network health check.
 # It will provide the system's public IP address, test the Ping Connection,
 # DNS resolution and check if the HTTPS port is reachable.
@@ -11,9 +12,8 @@
 set -e
 
 DEST="${1}"
-
-echo "====== Network Health Check ======"
-echo "Date: $(date)"
+OVERALL_STATUS=0
+FAIL_COUNT=0
 
 # 1. System public IP
 echo "Public IP collected: $(curl -s ifconfig.me || echo -e '\033[0;33mUnable to fetch')"
@@ -26,25 +26,38 @@ if [ -z "$DEST" ];then
 fi
 
 if ping -c 4 "$DEST" > /dev/null 2>&1; then
-        echo -e "\033[0;32mPing successful"
+        echo -e "✅ \033[0;32mPing successful"
 else
-        echo -e "\033[0;33mPing failed"
-fi
-
-# 3. DNS Resolution
-echo "DNS resolution for $DEST..."
-if dig +short "$DEST" > /dev/null; then
-        echo -e "\033[0;32mDNS resolution successful"
-else
-        echo -e "\033[0;33mDNS resolution failed"
-fi
-
-# 4. HTTPS reachability
-echo "Port 443 reachability for $DEST..."
-if nc -zv "$DEST" 443 2>&1 | grep -q "succeeded"; then
-        echo -e "\033[0;32mPort 443 is reachable."
-else
-        echo -e "\033[0;33mPort 443 is not reachable."
+        echo -e "❌  \033[0;33mPing failed"
+        ((FAIL_COUNT+=1))
 fi
 echo
-echo "====== Network Check Complete ======"
+
+# 3. DNS Resolution
+echo -e "\033[1;37mDNS resolution for $DEST..."
+if dig +short "$DEST" > /dev/null 2>&1; then
+        echo -e "✅ \033[0;32mDNS resolution successful"
+else
+        echo -e "❌ \033[0;33mDNS resolution failed"
+        ((FAIL_COUNT+=1))
+fi
+echo
+
+# 4. HTTPS reachability
+echo -e "\033[1;37mPort 443 reachability for $DEST..."
+if nc -zv -w 2 "$DEST" 443 2>&1 | grep -q "succeeded"; then
+        echo -e "✅ \033[0;32mPort 443 is reachable."
+else
+        echo -e "❌ \033[0;33mPort 443 is not reachable."
+        ((FAIL_COUNT+=1))
+fi
+echo
+echo -e "\033[1;37m--- Network Check Complete ---"
+
+if [ "$FAIL_COUNT" -eq 0 ];then
+        echo "✅ All network checks passed."
+        exit 0
+else
+        echo "❌ Some network checks failed."
+        exit 1
+fi
